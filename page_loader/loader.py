34e52976adac1dd61508430f60eb9019b1d24a12
Sergey Sakirko <ssakirko@mail.ru>
2020-11-16 22:33:02 +0300

    step 2

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -1,18 +1,49 @@
-from os import path
+import pathlib
 import re
+from os import path
+from urllib.parse import urljoin
+
 import requests
+from bs4 import BeautifulSoup
+
+FILES_FOLDER_POSTFIX = '_files'
+FILENAME_MAX_LENGTH = 255
 
 
-def prepare_file_name(url):
+def sanitize_string(string):
     pattern = r'[^\d\w]'
-    return f"{re.sub(pattern, '-', url.split('//')[1])}.html"
+    return f"{re.sub(pattern, '-', string)}"
+
+
+def remove_schema_from_url(url):
+    return url.split('//')[-1]
 
 
 def download(url, destination):
-    output_file_path = path.join(destination, prepare_file_name(url))
+    site_name = sanitize_string(remove_schema_from_url(url))
+    output_file_path = f'{path.join(destination, site_name)}.html'
+    file_folder_name = f'{site_name}{FILES_FOLDER_POSTFIX}'
+    file_folder_path = path.join(destination, file_folder_name)
+
+    pathlib.Path(file_folder_path).mkdir(parents=True)
 
     with open(output_file_path, 'w') as file:
         data = requests.get(url)
-        file.write(data.text)
+        soup = BeautifulSoup(data.text, 'html.parser')
+
+        for img in soup.find_all('img'):
+            src = urljoin(url, img['src'])
+            img_data = requests.get(src).content
+            file_path, extension = path.splitext(remove_schema_from_url(src))
+            file_name = f'{sanitize_string(file_path)[:FILENAME_MAX_LENGTH]}' \
+                        f'{extension}'
+            img_file_path = path.join(file_folder_path, file_name)
+
+            img['src'] = path.join(file_folder_name, file_name)
+
+            with open(img_file_path, 'bw+') as img_file:
+                img_file.write(img_data)
+
+        file.write(soup.prettify())
 
     return output_file_path
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,5 +1,4 @@
 import os
-
 import pathlib
 import tempfile
 
@@ -10,25 +9,41 @@ from page_loader import download
 FIXTURES_FOLDER = pathlib.Path(__file__).parent.absolute().joinpath('fixtures')
 
 
-def _get_fixture_data(filename, folder=FIXTURES_FOLDER):
-    return _read_file(pathlib.Path.joinpath(folder, filename))
-
-
-def _read_file(path):
-    with open(path, 'r') as file:
+def _read_file(*path_parts, mode='r'):
+    with open(os.path.join(*path_parts), mode) as file:
         return file.read()
 
 
+def _assert_html_files_equal(file1_data, file2_data):
+    assert ''.join(file1_data.split()) == ''.join(file2_data.split())
+
+
 def test_page_loader():
-    url = 'http://some/site'
-    fixture_data = _get_fixture_data('web_page.html')
+    url = 'http://some.ru/site'
+    img_url = 'http://some.ru/assets/img.png'
+
+    web_page_data = _read_file(FIXTURES_FOLDER, 'web_page.html')
+    img_data = _read_file(FIXTURES_FOLDER, 'assets', 'img.png', mode='rb')
 
     with requests_mock.Mocker() as mock:
-        mock.get(url, text=fixture_data)
+        mock.get(url, text=web_page_data)
+        mock.get(img_url, content=img_data)
 
         with tempfile.TemporaryDirectory() as tmpdir:
             file_path = download(url, tmpdir)
 
-            assert file_path.split('/')[-1] == 'some-site.html'
+            img_folder = os.path.join(tmpdir, 'some-ru-site_files')
+
+            assert file_path.split('/')[-1] == 'some-ru-site.html'
             assert os.path.exists(file_path)
-            assert _read_file(file_path) == fixture_data
+            assert os.path.exists(
+                os.path.join(img_folder, 'some-ru-assets-img.png')
+            )
+            assert _read_file(
+                img_folder, 'some-ru-assets-img.png', mode='rb'
+            ) == img_data
+
+            _assert_html_files_equal(
+                _read_file(file_path),
+                _read_file(FIXTURES_FOLDER, 'expected_web_page.html')
+            )
